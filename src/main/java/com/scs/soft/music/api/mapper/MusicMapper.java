783211ff853d365952307519e4db3f25e9082772
czzeng-code <czzeng_ok@qq.com>
2020-04-02 11:43:51 +0800

    MusicMapper

--- src/main/java/com/scs/soft/music/api/mapper/MusicMapper.java
+++ src/main/java/com/scs/soft/music/api/mapper/MusicMapper.java
@@ -1,4 +1,25 @@
 package com.scs.soft.music.api.mapper;
 
+import com.scs.soft.music.api.domain.entity.Music;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * @author ZENG
+ */
 public interface MusicMapper {
+    /**
+     * 根据id查询音乐
+     * @param id
+     */
+    void queryById(Integer id);
+
+    /**
+     * 分页查询音乐
+     * @param offset
+     * @param page
+     * @return List
+     */
+    List<Music> queryByPage(int offset, int page);
 }
